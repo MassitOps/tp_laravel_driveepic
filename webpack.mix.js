82f42f38a9405a0dd9b055d5eb231b21b3851053
MassitOps <massitahissou@gmail.com>
2024-01-20 20:42:31 +0100

    Finally, I did it

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,10 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
        require('autoprefixer'),
-   ]);
+   ])
+   .babelConfig({
+    "presets": ["@babel/preset-env"]
+   });
 
 mix.webpackConfig({
     module: {
